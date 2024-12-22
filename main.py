e5d73c41f777a2fa8b4a2406c9c92372222a512c
Виктор <heymangod2003@gmail.com>
2024-12-22 06:50:13 +0500

    Бета версия проекта

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-def foo():
-    return 'hello, github!'
+import random
 
-print(foo())
+flag = True
+
+def random_num() -> int:
+    """Функция генерирует случайную цифру"""
+
+    num: int = random.randint(1, 9)
+    return num
+
+def respond(num: any) -> bool:
+    """В будущем будет реализована проверка вводимых пользователем данных"""
+    pass
+
+print("Добро пожаловать в игру <Угадай число>!!")
+print("Давай, загадывай, я уже готов!!")
+
+# цифра, которую "компьютер загадал"
+num_comp = random_num()
+
+while flag:
+    
+    
+    num_user = int(input())
+
+    # if respond(num_user):
+
+    if random_num() == num_user:
+        flag = False
+        print("Ты угадал!!")
+    else:
+        print("Ох, по идее близко")
